ddfcf8f0e282cde9f979c4f5b05154e3623d6298
Derya Kalender <derya.sdev@gmail.com>
2024-12-11 14:07:35 +0300

    Entity Framework practise project added.

--- CSharpEgitimKampi301.EFProject/Form1.cs
+++ CSharpEgitimKampi301.EFProject/Form1.cs
@@ -16,5 +16,54 @@ namespace CSharpEgitimKampi301.EFProject
         {
             InitializeComponent();
         }
+
+        EgitimKampiEFTravelDbEntities db = new EgitimKampiEFTravelDbEntities();
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            
+            var values = db.TblGuide.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            TblGuide guide = new TblGuide();
+            guide.GuideName = txtName.Text;
+            guide.GuideSurname= txtSurname.Text;
+            db.TblGuide.Add(guide);
+            db.SaveChanges();
+            MessageBox.Show("Rehber başarıyla eklendi.");
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var removeValue = db.TblGuide.Find(id);
+            db.TblGuide.Remove(removeValue);
+            db.SaveChanges();
+            MessageBox.Show("Rehber başarıyla silindi.");
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var updatedValue = db.TblGuide.Find(id);
+            updatedValue.GuideName = txtName.Text;
+            updatedValue.GuideSurname = txtSurname.Text;
+            db.SaveChanges();
+            MessageBox.Show("Rehber başarıyla güncellendi.","Uyarı",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+
+        }
+
+        private void btntGetById_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var values = db.TblGuide.Where(x=> x.GuideId == id).ToList();
+            dataGridView1.DataSource = values;
+
+        }
     }
 }
